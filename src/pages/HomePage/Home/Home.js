c8df02984590da7080ff7ecc9e308fe5754338c2
delwarJnu11 <delwar999393@gmail.com>
2023-01-11 21:20:20 +0600

    changes

--- src/pages/HomePage/Home/Home.js
+++ src/pages/HomePage/Home/Home.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import Services from '../Services/Services';
-import Products from '../Products/Products';
-import OfferBanner from '../OfferBanner/OfferBanner';
-import Reviews from '../Reviews/Reviews';
-import Blogs from '../Blogs/Blogs';
-import Footer from '../../Shared/Footer/Footer';
-import useProducts from '../../../hooks/useProducts';
+import React from "react";
 import FadeLoader from "react-spinners/FadeLoader";
+import useProducts from "../../../hooks/useProducts";
+import Footer from "../../Shared/Footer/Footer";
+import Banner from "../Banner/Banner";
+import Blogs from "../Blogs/Blogs";
+import OfferBanner from "../OfferBanner/OfferBanner";
+import Products from "../Products/Products";
+import Reviews from "../Reviews/Reviews";
+import Services from "../Services/Services";
 
 const Home = () => {
     const { products } = useProducts();
+
+    if (!products) {
+        return (
+            <div className="text-center">
+                <FadeLoader
+                    size={150}
+                    color={"#b57600"}
+                    speedMultiplier={1.5}
+                />
+            </div>
+        );
+    }
     return (
         <div>
-            {
-                products.length > 0 ? <>
-                    <Banner></Banner>
-                    <Services></Services>
-                    <Products></Products>
-                    <OfferBanner></OfferBanner>
-                    <Reviews></Reviews>
-                    <Blogs></Blogs>
-                    <Footer></Footer>
-                </> : <div className='text-center'>
-                    <FadeLoader size={150} color={"#b57600"} speedMultiplier={1.5} />
-                </div>
-            }
-
-
+            <>
+                <Banner></Banner>
+                <Services></Services>
+                <Products></Products>
+                <OfferBanner></OfferBanner>
+                <Reviews></Reviews>
+                <Blogs></Blogs>
+                <Footer></Footer>
+            </>
         </div>
     );
 };
